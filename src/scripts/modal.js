d3f29b2b53e518d2a30c496e28bdf1680215e289
AxI0mAn <z1979ua@gmail.com>
2023-12-14 11:44:26 +0200

    1. modal smooth open 2. btn close modal (span instead of svg)

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -4,15 +4,17 @@ export default function modal() {
   const btnsClose = document.querySelector('#close_now');
   for (let btn of btnsOpen) {
     btn.addEventListener('click', () => {
-      modal.style.display = 'block';
+      console.log(btn);
+      modal.classList.toggle('modal_show');
+      console.log(modal.classList);
     });
   }
   window.onclick = function (event) {
     if (event.target == modal) {
-      modal.style.display = 'none';
+      modal.classList.toggle('modal_show');
     }
   };
   btnsClose.addEventListener('click', () => {
-    modal.style.display = 'none';
+    modal.classList.toggle('modal_show');
   });
 }
