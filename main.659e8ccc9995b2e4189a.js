39bcbb9180708ed3026f9823a17f7dd798af9f43
AxI0mAn <z1979ua@gmail.com>
2023-12-15 13:57:49 +0200

    Updates

--- main.659e8ccc9995b2e4189a.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";!function(){const e=document.querySelector(".modal"),n=document.querySelectorAll(".open_now"),o=document.querySelector("#close_now");for(let o of n)o.addEventListener("click",(()=>{e.style.display="block"}));window.onclick=function(n){n.target==e&&(e.style.display="none")},o.addEventListener("click",(()=>{e.style.display="none"}))}()}();
