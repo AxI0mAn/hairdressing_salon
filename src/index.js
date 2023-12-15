b5d27b6aca4664cca7c6767a86378a7257e9a687
AxI0mAn <z1979ua@gmail.com>
2023-12-15 12:09:57 +0200

    correct review-page

--- src/index.js
+++ src/index.js
@@ -7,3 +7,17 @@ import './styles/view/index.scss';
 //
 import modal from './scripts/modal.js';
 modal();
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+new Swiper('.swiper-review', {
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.btn_review_right',
+    prevEl: '.btn_review_left',
+  },
+});
